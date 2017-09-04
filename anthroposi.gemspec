d22143936b09b0fe559880f70578d79e0086fd38
KZWin <me@none>
2017-09-03 21:14:04 -0400

    initial commit

---
+++ anthroposi.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'anthroposi'
+  s.version     = File.open('makefile', &:readline).chomp.split('=')[1]
+  s.licenses    = ['MIT']
+  s.summary     = 'Turn number in binary units such as MiB, KiB or decimal prefixes G, T'
+  s.description = ''
+  s.authors     = ['k z win']
+  s.email       = 'rubygems@happyw.info'
+  s.files       = Dir['lib/*']
+  s.homepage    = 'https://rubygems.org/gems/example'
+end
---
+++ lib/anthroposi.rb
@@ -0,0 +1,60 @@
+class Anthroposi
+  SI_PREFIXES = %w(K M G T P E Z Y).freeze
+  BINARY_UNITS = ['B'] + SI_PREFIXES.map { |p| "#{p}iB" }
+  DECIMAL_PREFIXES = [''] + SI_PREFIXES
+  KILO = 1024
+
+  def initialize(bytes, decimal = false)
+    raise(ArgumentError, 'no string or fractional number') if bytes > 0 && bytes < 1
+    @bytes = bytes
+    @bytes.zero? and return handle_zero(decimal)
+    prepare(decimal)
+    @unit = compute
+  end
+
+  def to_s
+    format @mantissa > 9999 ? '%.2e%s' : '%s%s', @mantissa, @unit
+  end
+
+  private
+
+  def prepare(decimal)
+    if decimal
+      @thousand = 1000
+      @units = DECIMAL_PREFIXES
+      @ordinal = Math.log10(@bytes).to_i / 3
+    else
+      @thousand = KILO
+      @units = BINARY_UNITS
+      @ordinal = Math.log2(@bytes).to_i / 10
+    end
+  end
+
+  def handle_zero(decimal)
+    @mantissa = 0
+    @units = decimal ? DECIMAL_PREFIXES : BINARY_UNITS
+    @unit = @units[0]
+  end
+
+  def compute
+    if @ordinal < BINARY_UNITS.size
+      @mantissa = significant_digits(@bytes.to_f / @thousand**@ordinal, 3)
+      return @units[@ordinal]
+    end
+    @mantissa = significant_digits(@bytes.to_f / @thousand**SI_PREFIXES.size, 3)
+    @units[-1]
+  end
+end
+
+def significant_digits(number, digits)
+  return 0 if number.zero? || digits.zero?
+  number.to_f.round(digits - 1 - Math.log10(number.abs).floor)
+end
+
+def human_size(b)
+  Anthroposi.new(b).to_s
+end
+
+def human_decimal_size(b)
+  Anthroposi.new(b, true).to_s
+end
